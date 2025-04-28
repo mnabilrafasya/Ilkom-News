77b2b09df0a87ba182d51a2545551b3d4166a6c9
Multimaxs <jordiabrar@gmail.com>
2025-04-28 14:17:48 +0700

    link navbar

--- client/src/components/ComponentUtama/Navbar.jsx
+++ client/src/components/ComponentUtama/Navbar.jsx
@@ -6,43 +6,49 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarInner}>
-      <div className={styles.navbarLogo}>
-        <img
-          src="/logoBEM.png"
-          alt="Logo Artha Darma"
-          className={styles.logoImage}
-        />
-        <div className={styles.navbarText}>
-          <span className={styles.decorativeBold}>A</span>
-          <span className={styles.regular}>RTHA</span>{" "}
-          <span className={styles.regular}>DARM</span>
-          <span className={styles.decorativeBold}>A</span>
-          <br />
-          BEM KM FASILKOM UNSRI
+        <div className={styles.navbarLogo}>
+          <img
+            src="/logoBEM.png"
+            alt="Logo Artha Darma"
+            className={styles.logoImage}
+          />
+          <div className={styles.navbarText}>
+            <span className={styles.decorativeBold}>A</span>
+            <span className={styles.regular}>RTHA</span>{" "}
+            <span className={styles.regular}>DARM</span>
+            <span className={styles.decorativeBold}>A</span>
+            <br />
+            BEM KM FASILKOM UNSRI
+          </div>
         </div>
-      </div>
-      <ul className={styles.navbarMenu}>
-        <li>
-          <a href="/" className={styles.navbarLink}>
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className={styles.navbarLink}>
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#" className={styles.navbarLink}>
-            Profile
-          </a>
-        </li>
-        <li>
-          <a href="#" className={styles.navbarLink}>
-            BEM Apps
-          </a>
-        </li>
-      </ul>
+        <ul className={styles.navbarMenu}>
+          <li>
+            <a href="/" className={styles.navbarLink}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.bemilkomunsri.org/about"
+              className={styles.navbarLink}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.bemilkomunsri.org/profile"
+              className={styles.navbarLink}
+            >
+              Profile
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.navbarLink}>
+              BEM Apps
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
